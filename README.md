92224f737ed57c50b51909fa33565790c7543db6
GhazanfarShahbaz <ghazanfarshahbaz2409@gmail.com>
2024-06-29 17:10:12 -0400

    add

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # Obsidian-3DKnowledgeGraph
 
+# To run locall
+```
+npx vite
+```
+
 # To Create Deployment Directory
 Run npx vite build
--- main.jsx
+++ main.jsx
@@ -3,7 +3,6 @@ import ForceGraph3D from '3d-force-graph';
 
 const renderer = new THREE.WebGLRenderer();
 
-import data from './data.json';
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
@@ -13,12 +12,31 @@ const camera = new THREE.PerspectiveCamera();
 
 camera.position.z = 5;
 
-const graph = ForceGraph3D()
+const dataUrl = import.meta.env.VITE_OBSIDIAN_URL
+const param = import.meta.env.VITE_OBSIDIAN_PARAM
+
+// Extract the query string from the URL
+const queryString = window.location.search;
+
+// Use URLSearchParams to parse the query string
+const urlParams = new URLSearchParams(queryString);
+const vaultName = urlParams.get(param); // Assuming there's a "user" parameter in the URL
+
+
+let jsonUrl = `${dataUrl}`
+
+if (vaultName !== null) {
+  jsonUrl = `${dataUrl}?${param}=${vaultName}`
+}
+
+console.log(import.meta.env.VITE_SOME_KEY) // "123"
+
+ForceGraph3D()
   (document.getElementById('3d-graph'))
-    .nodeAutoColorBy('name')
-    .nodeOpacity(1)
-    .nodeRelSize(2)
-    .linkAutoColorBy('name')
-    .linkOpacity(1)
-    .backgroundColor('#636363')
-    .graphData(data);
+  .nodeAutoColorBy('name')
+  .nodeOpacity(1)
+  .nodeRelSize(2)
+  .linkAutoColorBy('name')
+  .linkOpacity(1)
+  .backgroundColor('#636363')
+  .jsonUrl(jsonUrl);
